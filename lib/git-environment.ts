de0be4590bdbdf724678b27f6a632a9fb92c1a62
Jesse Bennett <jesse.alexander.bennett@gmail.com>
2020-08-22 06:03:56 +0000

    updates for freebsd

--- lib/git-environment.ts
+++ lib/git-environment.ts
@@ -1,18 +1,19 @@
 import * as path from 'path'
 
 function resolveEmbeddedGitDir(): string {
-  if (
-    process.platform === 'darwin' ||
-    process.platform === 'linux' ||
-    process.platform === 'android' ||
-    process.platform === 'win32'
-  ) {
-    const s = path.sep
-    return path
-      .resolve(__dirname, '..', '..', 'git')
-      .replace(/[\\\/]app.asar[\\\/]/, `${s}app.asar.unpacked${s}`)
-  }
-  throw new Error('Git not supported on platform: ' + process.platform)
+    if (
+        process.platform === 'darwin' ||
+        process.platform === 'linux' ||
+        process.platform === 'android' ||
+        process.platform === 'win32' ||
+        process.platform === 'freebsd'
+    ) {
+        const s = path.sep
+        return path
+            .resolve(__dirname, '..', '..', 'git')
+            .replace(/[\\\/]app.asar[\\\/]/, `${s}app.asar.unpacked${s}`)
+    }
+    throw new Error('Git not supported on platform: ' + process.platform)
 }
 
 /**
@@ -22,23 +23,23 @@ function resolveEmbeddedGitDir(): string {
  *  returns with it after resolving it as a path.
  */
 function resolveGitDir(): string {
-  if (process.env.LOCAL_GIT_DIRECTORY != null) {
-    return path.resolve(process.env.LOCAL_GIT_DIRECTORY)
-  } else {
-    return resolveEmbeddedGitDir()
-  }
+    if (process.env.LOCAL_GIT_DIRECTORY != null) {
+        return path.resolve(process.env.LOCAL_GIT_DIRECTORY)
+    } else {
+        return resolveEmbeddedGitDir()
+    }
 }
 
 /**
  *  Find the path to the embedded Git binary.
  */
 function resolveGitBinary(): string {
-  const gitDir = resolveGitDir()
-  if (process.platform === 'win32') {
-    return path.join(gitDir, 'cmd', 'git.exe')
-  } else {
-    return path.join(gitDir, 'bin', 'git')
-  }
+    const gitDir = resolveGitDir()
+    if (process.platform === 'win32') {
+        return path.join(gitDir, 'cmd', 'git.exe')
+    } else {
+        return path.join(gitDir, 'bin', 'git')
+    }
 }
 
 /**
@@ -48,19 +49,19 @@ function resolveGitBinary(): string {
  * then it returns with it after resolving it as a path.
  */
 function resolveGitExecPath(): string {
-  if (process.env.GIT_EXEC_PATH != null) {
-    return path.resolve(process.env.GIT_EXEC_PATH)
-  }
-  const gitDir = resolveGitDir()
-  if (process.platform === 'win32') {
-    if (process.arch === 'x64') {
-      return path.join(gitDir, 'mingw64', 'libexec', 'git-core')
+    if (process.env.GIT_EXEC_PATH != null) {
+        return path.resolve(process.env.GIT_EXEC_PATH)
     }
+    const gitDir = resolveGitDir()
+    if (process.platform === 'win32') {
+        if (process.arch === 'x64') {
+            return path.join(gitDir, 'mingw64', 'libexec', 'git-core')
+        }
 
-    return path.join(gitDir, 'mingw32', 'libexec', 'git-core')
-  } else {
-    return path.join(gitDir, 'libexec', 'git-core')
-  }
+        return path.join(gitDir, 'mingw32', 'libexec', 'git-core')
+    } else {
+        return path.join(gitDir, 'libexec', 'git-core')
+    }
 }
 
 /**
@@ -72,63 +73,63 @@ function resolveGitExecPath(): string {
  * @param additional options to include with the process
  */
 export function setupEnvironment(
-  environmentVariables: NodeJS.ProcessEnv
+    environmentVariables: NodeJS.ProcessEnv
 ): { env: NodeJS.ProcessEnv; gitLocation: string } {
-  const gitLocation = resolveGitBinary()
+    const gitLocation = resolveGitBinary()
 
-  let envPath: string = process.env.PATH || ''
-  const gitDir = resolveGitDir()
+    let envPath: string = process.env.PATH || ''
+    const gitDir = resolveGitDir()
 
-  if (process.platform === 'win32') {
-    if (process.arch === 'x64') {
-      envPath = `${gitDir}\\mingw64\\bin;${gitDir}\\mingw64\\usr\\bin;${envPath}`
-    } else {
-      envPath = `${gitDir}\\mingw32\\bin;${gitDir}\\mingw32\\usr\\bin;${envPath}`
+    if (process.platform === 'win32') {
+        if (process.arch === 'x64') {
+            envPath = `${gitDir}\\mingw64\\bin;${gitDir}\\mingw64\\usr\\bin;${envPath}`
+        } else {
+            envPath = `${gitDir}\\mingw32\\bin;${gitDir}\\mingw32\\usr\\bin;${envPath}`
+        }
     }
-  }
 
-  const env = Object.assign(
-    {},
-    process.env,
-    {
-      GIT_EXEC_PATH: resolveGitExecPath(),
-      PATH: envPath
-    },
-    environmentVariables
-  )
+    const env = Object.assign(
+        {},
+        process.env,
+        {
+            GIT_EXEC_PATH: resolveGitExecPath(),
+            PATH: envPath
+        },
+        environmentVariables
+    )
 
-  if (process.platform === 'win32') {
-    // while reading the environment variable is case-insensitive
-    // you can create a hash with multiple values, which means the
-    // wrong value might be used when spawning the child process
-    //
-    // this ensures we only ever supply one value for PATH
-    if (env.Path) {
-      delete env.Path
+    if (process.platform === 'win32') {
+        // while reading the environment variable is case-insensitive
+        // you can create a hash with multiple values, which means the
+        // wrong value might be used when spawning the child process
+        //
+        // this ensures we only ever supply one value for PATH
+        if (env.Path) {
+            delete env.Path
+        }
     }
-  }
 
-  if (process.platform === 'darwin' || process.platform === 'linux') {
-    // templates are used to populate your .git folder
-    // when a repository is initialized locally
-    const templateDir = `${gitDir}/share/git-core/templates`
-    env.GIT_TEMPLATE_DIR = templateDir
-  }
-
-  if (process.platform === 'linux') {
-    // when building Git for Linux and then running it from
-    // an arbitrary location, you should set PREFIX for the
-    // process to ensure that it knows how to resolve things
-    env.PREFIX = gitDir
-
-    if (!env.GIT_SSL_CAINFO && !env.LOCAL_GIT_DIRECTORY) {
-      // use the SSL certificate bundle included in the distribution only
-      // when using embedded Git and not providing your own bundle
-      const distDir = resolveEmbeddedGitDir()
-      const sslCABundle = `${distDir}/ssl/cacert.pem`
-      env.GIT_SSL_CAINFO = sslCABundle
+    if (process.platform === 'darwin' || process.platform === 'linux' || process.platform === 'freebsd') {
+        // templates are used to populate your .git folder
+        // when a repository is initialized locally
+        const templateDir = `${gitDir}/share/git-core/templates`
+        env.GIT_TEMPLATE_DIR = templateDir
     }
-  }
 
-  return { env, gitLocation }
+    if (process.platform === 'linux') {
+        // when building Git for Linux and then running it from
+        // an arbitrary location, you should set PREFIX for the
+        // process to ensure that it knows how to resolve things
+        env.PREFIX = gitDir
+
+        if (!env.GIT_SSL_CAINFO && !env.LOCAL_GIT_DIRECTORY) {
+            // use the SSL certificate bundle included in the distribution only
+            // when using embedded Git and not providing your own bundle
+            const distDir = resolveEmbeddedGitDir()
+            const sslCABundle = `${distDir}/ssl/cacert.pem`
+            env.GIT_SSL_CAINFO = sslCABundle
+        }
+    }
+
+    return { env, gitLocation }
 }
